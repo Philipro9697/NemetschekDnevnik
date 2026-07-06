6687b0df78279b22f411338f6bb4374570c075f4
Aleksandar Stanchev <altryx15@gmail.com>
2026-07-06 17:39:40 +0300

    a dto + unfinished middleware

--- NemetschekDnevnik.Server/Middleware/EnsureApprovedUserMiddleware.cs
+++ NemetschekDnevnik.Server/Middleware/EnsureApprovedUserMiddleware.cs
@@ -2,4 +2,8 @@ public class EnsureApprovedUserMiddleware
 {
     private readonly RequestDelegate _next;
     
+    public EnsureApprovedUserMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
 }
