cd0922a3806dadf36da9760040df977571d41409
Philipro9697 <93782015+Philipro9697@users.noreply.github.com>
2026-07-06 15:16:04 +0300

    fixed model namespaces

---
+++ NemetschekDnevnik.Server/Models/Attendance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NemetschekDnevnik.Server.Models;
+
+public partial class Attendance
+{
+    public int LessonId { get; set; }
+
+    public int StudentId { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public virtual WeeklySchedule Lesson { get; set; } = null!;
+
+    public virtual Student Student { get; set; } = null!;
+}
