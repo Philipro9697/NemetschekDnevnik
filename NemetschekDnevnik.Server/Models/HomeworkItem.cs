e3dbf9f517729bd594ad7a9c19affefe7f5ae92a
M.Venelinova <miroslavavenelinova4@gmail.com>
2026-07-07 14:51:46 +0300

    Add new files

--- NemetschekDnevnik.Server/Models/HomeworkItem.cs
+++ NemetschekDnevnik.Server/Models/HomeworkItem.cs
@@ -11,11 +11,23 @@ public partial class HomeworkItem
 
     public int? SubjectId { get; set; }
 
-    public DateOnly DateDue { get; set; }
+    public int? TeacherId { get; set; }
 
-    public string? SubjectLink { get; set; }
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public DateTime DateAssigned { get; set; }
+
+    public DateTime DateDue { get; set; }
+
+    public string? ResourceLink { get; set; }
 
     public virtual Class? Class { get; set; }
 
     public virtual Subject? Subject { get; set; }
+
+    public virtual ICollection<SubmittedHomework> SubmittedHomeworks { get; set; } = new List<SubmittedHomework>();
+
+    public virtual Teacher? Teacher { get; set; }
 }
--- NemetschekDnevnik.Server/Models/Parent.cs
+++ NemetschekDnevnik.Server/Models/Parent.cs
@@ -7,12 +7,6 @@ public partial class Parent
 {
     public int ParentId { get; set; }
 
-    public string FirstName { get; set; } = null!;
-
-    public string LastName { get; set; } = null!;
-
-    public string PhoneNumber { get; set; } = null!;
-
     public virtual User ParentNavigation { get; set; } = null!;
 
     public virtual ICollection<Student> Students { get; set; } = new List<Student>();
