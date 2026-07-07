837ced2f4e2d380676f29e03e40168dd5d2894d2
polimerto360 <ivan@kolevi.net>
2026-07-07 14:55:17 +0300

    fixed running

--- NemetschekDnevnik.Server/Migrations/20260707114226_test2.Designer.cs
+++ NemetschekDnevnik.Server/Migrations/20260707114226_test2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NemetschekDnevnik.Server.Models;
 
@@ -11,9 +12,11 @@ using NemetschekDnevnik.Server.Models;
 namespace NemetschekDnevnik.Server.Migrations
 {
     [DbContext(typeof(NemetschekSchoolDiaryContext))]
-    partial class NemetschekSchoolDiaryContextModelSnapshot : ModelSnapshot
+    [Migration("20260707114226_test2")]
+    partial class test2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
