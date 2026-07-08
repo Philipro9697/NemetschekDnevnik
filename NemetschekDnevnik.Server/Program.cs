4e51e389d2ecae5a97a4e3762712551ab11cf0b3
Philipro9697 <93782015+Philipro9697@users.noreply.github.com>
2026-07-08 14:07:23 +0300

    Everything is done.

--- NemetschekDnevnik.Server/Program.cs
+++ NemetschekDnevnik.Server/Program.cs
@@ -74,7 +74,7 @@ app.MapStaticAssets();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthentication();
 //app.UseMiddleware<EnsureApprovedUserMiddleware>();
