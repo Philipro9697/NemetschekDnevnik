6dc1a14445481afb76f5a37f868672f5b5ab00c1
Philipro9697 <93782015+Philipro9697@users.noreply.github.com>
2026-07-09 11:41:13 +0300

    connection. Base files.

---
+++ nemetschekdnevnik.client/api/types.ts
@@ -0,0 +1,112 @@
+export type UserRole = 'Teacher' | 'Student' | 'Parent';
+
+export interface AbsenceDto {
+    isExcused: boolean;
+    date: string; // "YYYY-MM-DD"
+    time: string; // "HH:mm:ss"
+    subjectId: number;
+    subjectName: string;
+    lessonId: number;
+}
+
+export interface CreateUserDto {
+    email: string;
+    password: string;
+    role: UserRole;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+}
+
+export interface GradeDto {
+    gradeValue: number;
+    subjectId: number;
+    teacherId: number;
+    subjectName: string;
+    teacherFirstName: string;
+    teacherLastName: string;
+    gradeTypeName: string;
+    comment?: string | null;
+    entryDate: string;
+}
+
+export interface HomeworkItemDto {
+    homeworkId: number;
+    subjectId: number;
+    teacherId: number;
+    title: string;
+    descritpion: string; //fun typo
+    resourceLink: string;
+    dateAssigned: string;
+    dateDue: string;
+}
+
+export interface LessonDto {
+    date: string;
+    time: string;
+    subjectId: number;
+    teacherId: number;
+    teacherFirstName: string;
+    teacherLastName: string;
+    subjectName: string;
+    scheduleItemId: number;
+}
+
+export interface LoginDto {
+    email: string;
+    password: string;
+}
+
+export interface RegisterDto {
+    email: string;
+    password: string;
+    role: UserRole;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+}
+
+export interface RemarkDto {
+    teacherId: number;
+    dateCreated: string;
+    type: string;
+    text: string;
+    teacherFirstName: string;
+    teacherLastName: string;
+}
+
+export interface ScheduleDto {
+    dayOfWeek: number;
+    time: string;
+    subjectId: number;
+    teacherId: number;
+    teacherFirstName: string;
+    teacherLastName: string;
+    subjectName: string;
+    location?: string | null;
+}
+
+export interface SubjectDto {
+    subjectId: number;
+    subjectName: string;
+}
+
+export interface UserAccountDto {
+    userId: number;
+    email: string;
+    role: UserRole | string;
+    isApproved: boolean;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+}
+
+export interface StudentInfoDto {
+    studentId: number;
+    parentId: number;
+    classId: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneNumber: string;
+}
